559393c3a66f77e7a856855b7b50d1a6b919555f
qijinbiao <1025171835@qq.com>
2023-08-13 19:38:58 +0800

    Update Tools.h
    添加enum

--- ModelWork/ModelWork/Tools.h
+++ ModelWork/ModelWork/Tools.h
@@ -11,6 +11,22 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
+enum enCols
+{
+	clName = 1,		//股票名称
+	clCode,			//股票代码
+	clDate,			//日期
+	clOpen,			//开盘
+	clClose,		//收盘
+	clHigh,			//最高
+	clLow,			//最低
+	clVolume,		//成交量
+	clTraVol,		//成交额
+	clAmplitude,	//振幅
+	clFluctuation,	//涨跌幅
+	clPriceFluc,	//涨跌额
+	clTurnoverRate	//换手率
+};
 enum enUpLowStop
 {
 	iOpenUpStop = 1,
@@ -18,10 +34,10 @@ enum enUpLowStop
 	iCloseUpStop = 4,
 	iCloseLowStop = 8
 };
-int DoubleToInt(double db);
-double StrToDouble(string str);
 string IntToStr(int ix);
 int StrToInt(string str, int iradix = 10);
+int DoubleToInt(double db);
+double StrToDouble(string str);
 string DoubleToStr(double dbValue);
 void StrReplace(string &str, string src, string des);
 void Split(const string &strInfo, vector<string> &vctInfo, char ch);
